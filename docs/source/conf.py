fc9af0232b8e2c04d550a83844a0ec11480c2306
corinnar <corinna.ravilious@unep-wcmc.org>
2023-11-05 17:54:38 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,9 +42,9 @@ import sphinx_rtd_theme
 
 
 # -- Options for HTML output
-#extensions = [
-#    'sphinx_rtd_theme',
-#]
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 #html_theme = 'sphinx_rtd_theme'
 
