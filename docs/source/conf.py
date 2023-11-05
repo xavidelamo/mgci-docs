e13b5e7d02ccd79c6fb9ede7d359387943ab3312
corinnar <corinna.ravilious@unep-wcmc.org>
2023-11-05 16:42:00 +0000

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,16 +48,6 @@ extensions = [
 
 html_theme = 'sphinx_rtd_theme'
 
-html_style = 'css/custom.css'
-
-# These folders are copied to the documentation's HTML output
-html_static_path = ['_static']
-
-# These paths are either relative to html_static_path
-# or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/custom.css',
-]
 
 
 
